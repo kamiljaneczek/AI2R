a215da3d0499be22ef6b33ea12fe346a542d2804
kamiljaneczek <janeczekkamil@gmail.com>
2024-04-06 21:27:53 +0200

    whoami done

--- src/excercises/whoami.ts
+++ src/excercises/whoami.ts
@@ -7,28 +7,48 @@ import {
   HumanMessage,
   SystemMessage,
 } from "langchain/schema";
-import { context } from "../samples/02_langchain_format/02_context";
 
 async function main() {
-  const token = await authorize("scraper");
+  const token = await authorize("whoami");
 
-  const task = await getQuestion(token!);
-  const url = task.input! as string;
-  const question = task.question! as string;
-
-  const result = await fetch(url, { method: "GET" });
-  const context = await result.text();
-  const chat = new ChatOpenAI();
-  const message = new HumanMessage(question);
-  const systemPrompt = `Basing on information in Context answer user question. Limit your answer to 130 characters.
-  ~~~~~~~~~~
-  Context:
-  ${context}`;
+  const systemPrompt = `Basing on hints provided by user guess the character being described. 
+  Answer with character name only when you are fully and absolutely certain of your answer.
+  Before that as for one more hint by typing HINT, to double chekc your are rigth.
+   Otherwise just answer HINT to get another hint.`;
 
   const systemMessage = new SystemMessage(systemPrompt);
-  const answer = await chat.invoke([systemMessage, message]);
-  console.log("answer :", answer.lc_kwargs.content);
-  const response = await setAnswer(token!, answer.lc_kwargs.content);
+  const messages = [systemMessage];
+  let answer: TAnswer = {
+    lc_kwargs: {
+      content: "",
+    },
+  };
+
+  type TAnswer = {
+    lc_kwargs?: {
+      content: string;
+    };
+  };
+  const chat = new ChatOpenAI({ modelName: "gpt-4-turbo-preview" });
+  let person = "";
+  let bContinue = true;
+  while (bContinue) {
+    const task = await getQuestion(token!);
+    const hint = task.hint! as string;
+
+    const hintMessage = new HumanMessage(hint);
+    messages.push(hintMessage);
+    const answer = await chat.invoke(messages);
+    console.log("answer :", answer.lc_kwargs.content);
+
+    if (answer.lc_kwargs.content !== "HINT") {
+      bContinue = false;
+      person = answer.lc_kwargs!.content;
+    }
+  }
+
+  console.log("answer this person is:", person);
+  const response = await setAnswer(token!, person);
   console.log("response :", response);
 }
 
